d0ccadd8eae99c0f44fdc5bc7acdb74c4e9fc3b6
eraices <eraices99@gmail.com>
2026-06-12 21:40:24 -0400

    Added GameEngine and GamePanel

---
+++ src/main/java/com/github/eraices/core/GameEngine.java
@@ -0,0 +1,40 @@
+package com.github.eraices.core;
+
+import javax.swing.JFrame;
+
+public class GameEngine {
+
+	// The game will calculate based on these dimensions,
+	// regardless of the actual window dimensions
+	public static final int VIRTUAL_SCREEN_WIDTH = 800;
+	public static final int VIRTUAL_SCREEN_HEIGHT = 600;
+
+	private JFrame window;
+
+	public GameEngine() {
+		initializeWindow();
+	}
+
+	public static void main(String[] args)  {
+		// Force rendering to ignore OS scaling
+		System.setProperty("sun.java2d.uiScale", "1");
+
+		new GameEngine();
+	 }
+
+	private void initializeWindow() {
+		window = new JFrame();
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setTitle("Survival Game");
+		window.setResizable(true);
+
+		GamePanel gamePanel = new GamePanel();
+		window.add(gamePanel);
+		window.pack();
+
+		window.setLocationRelativeTo(null);
+		window.setVisible(true);
+		
+		gamePanel.startGameThread();
+	}
+}
---
+++ src/main/java/com/github/eraices/core/GamePanel.java
@@ -0,0 +1,74 @@
+package com.github.eraices.core;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel implements Runnable {
+    
+	private final int FPS = 60;
+	private final double DRAW_INTERVAL = 1000000000.0 / FPS;
+
+    private Thread gameThread;
+	
+	public GamePanel() {
+		this.setPreferredSize(new Dimension(GameEngine.VIRTUAL_SCREEN_WIDTH, GameEngine.VIRTUAL_SCREEN_HEIGHT));
+		this.setDoubleBuffered(true); // Basically improves performance
+		this.setBackground(Color.BLACK);
+		this.setFocusable(true);
+	}
+
+	public void startGameThread() {
+		gameThread = new Thread(this);
+		gameThread.start();
+	}
+	
+	@Override
+	public void run() {
+		double delta = 0;
+		long lastTime = System.nanoTime();
+		long currentTime;
+		long timer = 0;
+		int drawCount = 0;
+		int frameNum = 1;
+		
+		// This loop will keep running as long as this thread exists
+		while(gameThread != null) {
+			currentTime = System.nanoTime();
+			delta += (currentTime - lastTime) / DRAW_INTERVAL;
+			timer += (currentTime - lastTime);
+			lastTime = currentTime;
+			
+			if(delta >= 1) {
+				update();
+				repaint();
+				
+				delta = 0;
+				drawCount++;
+			}
+			
+			if(timer >= 1000000000) {
+				System.out.println("FPS (" + frameNum + "): " + drawCount);
+				drawCount = 0;
+				timer = 0;
+				frameNum++;
+			}
+		}
+	}
+
+	public void update() {
+    	// TODO: Implement update stuff
+	}
+
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		Graphics2D g2 = (Graphics2D)g;
+
+		// TODO: Implement drawing stuff
+
+		g.dispose(); // Clear up graphics resources efficiently
+    }
+}
