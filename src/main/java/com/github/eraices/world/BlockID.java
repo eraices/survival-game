91a2cb5c4d73f038b65f3c1682ccefedab645081
eraices <eraices99@gmail.com>
2026-06-17 12:03:50 -0400

    Added more blocks

--- src/main/java/com/github/eraices/world/BlockID.java
+++ src/main/java/com/github/eraices/world/BlockID.java
@@ -1,9 +1,12 @@
 package com.github.eraices.world;
 
 public final class BlockID {
+    public static final int NUM_BLOCKS = 100;
     public static final int AIR = 0;
     public static final int GRASS = 1;
     public static final int DIRT = 2;
     public static final int SAND = 3;
-    public static final int WATER = 4;
+    public static final int TREE = 4;
+    public static final int STONE = 5;
+    public static final int WATER = 6;
 }
